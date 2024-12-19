65876dfcd832efceed9f67b45ffae672d5e477a9
Ly Sochetra <lysochetra95@gmail.com>
2024-12-19 10:40:54 +0700

    first commit

---
+++ src/main/java/Oggy/Oggy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Oggy;
+
+/**
+ *
+ * @author ahpka
+ */
+public class Oggy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
